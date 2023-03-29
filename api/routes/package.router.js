7570f72a244f819a60a70836942bb9fd0f6cc5c5
AdayOrtega <aday.ortega@gmail.com>
2023-03-29 12:13:07 +0100

    GENERAL
    Packages routes with middleware done

--- api/routes/package.router.js
+++ api/routes/package.router.js
@@ -1,12 +1,30 @@
 const router = require('express').Router()
+const { checkAuth, checkAdmin } = require('../middleware/auth')
 
 const { getAllPackages, getOnePackage, createOnePackage, updatePackage, deletePackage } = require('../controllers/package.controller')
 
 
-router.get('/', getAllPackages)
-router.get('/:id', getOnePackage)
-router.post('/', createOnePackage)
-router.put('/:id', updatePackage)
-router.delete('/:id', deletePackage)
+router.get('/', checkAuth, getAllPackages, (req, res) => {
+    res.send('Aquí se muestran todos los paquetes')
+})
+
+router.get('/:id', checkAuth, getOnePackage, (req, res) =>{
+    res.send('Aquí se muestra un solo paquete')
+})
+
+router.post('/', checkAuth, checkAdmin, createOnePackage, (req, res) => {
+    res.send('Aquí el administrador crea un paquete')
+})
+
+router.put('/:id', checkAuth, checkAdmin, updatePackage, (req, res) => {
+    res.send('Aquí el administrador actualiza el paquete')
+})
+
+router.delete('/:id', checkAuth, checkAdmin, deletePackage, (req, res) => {
+    res.send('Aqui el administrador elimina el paquete')
+})
+
+
+
 
 module.exports = router
